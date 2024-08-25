d496ec8c3f2e3d1d3dea6e80860d49520e58ca6a
Tatiana Stulova <vitadbnf@mail.ru>
2024-08-25 11:23:35 +0300

    add diplom

--- src/test/java/api/models/event/CreateEventTemplateRequestModel.java
+++ src/test/java/api/models/event/CreateEventTemplateRequestModel.java
@@ -1,14 +1,15 @@
 package api.models.event;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AllArgsConstructor;
 import lombok.Data;
-
+@AllArgsConstructor
 @Data
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class CreateEventTemplateRequestModel {
 
     String name;
     AccessSettingsModel accessSettings;
-
+    @AllArgsConstructor
     @Data
     @JsonIgnoreProperties(ignoreUnknown = true)
     public static class AccessSettingsModel {
--- src/test/java/tests/api/EventTests.java
+++ src/test/java/tests/api/EventTests.java
@@ -2,17 +2,21 @@ package tests.api;
 
 import api.models.event.CreateEventTemplateRequestModel;
 import api.models.event.CreateEventTemplateResponseModel;
+import common.data.TestData;
 import io.qameta.allure.Severity;
 import io.qameta.allure.SeverityLevel;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static api.specs.Specs.requestSpec;
+import static api.specs.Specs.responseSpecStatusCode201;
 import static io.qameta.allure.Allure.step;
 import static io.restassured.RestAssured.sessionId;
 import static org.junit.jupiter.api.Assertions.*;
 import static io.restassured.RestAssured.given;
 public class EventTests extends TestBaseApi{
+    TestData testData = new TestData();
+
     @Test
     @DisplayName("Создание шаблона для мероприятия")
     @Severity(SeverityLevel.BLOCKER)
@@ -28,7 +32,7 @@ public class EventTests extends TestBaseApi{
                         .when()
                         .post("/event")
                         .then()
-                        .spec(responseSpecStatusCode201)
+                        .spec(responseSpecStatusCode201))
                 .extract().as(CreateEventTemplateResponseModel.class);
 
         step("Проверить данные в ответе", () -> {
