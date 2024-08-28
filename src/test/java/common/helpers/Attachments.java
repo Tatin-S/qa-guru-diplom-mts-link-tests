e8a6e4c0ea351e06f2d12918c32966cf491b885c
Tatiana Stulova <vitadbnf@mail.ru>
2024-08-28 17:46:03 +0300

    change EventTests

--- src/test/java/common/helpers/Attachments.java
+++ src/test/java/common/helpers/Attachments.java
@@ -1,4 +1,4 @@
-package helpers;
+package common.helpers;
 
 import com.codeborne.selenide.Selenide;
 import io.qameta.allure.Attachment;
@@ -13,17 +13,10 @@ import static com.codeborne.selenide.Selenide.sessionId;
 import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
 import static org.openqa.selenium.logging.LogType.BROWSER;
 
-public class Attach {
+public class Attachments {
     @Attachment(value = "{attachName}", type = "image/png")
     public static byte[] screenshotAs(String attachName) {
         return ((TakesScreenshot) getWebDriver()).getScreenshotAs(OutputType.BYTES);
-    }
-
-    @Attachment(value = "Video", type = "text/html", fileExtension = ".html")
-    public static String addVideo(String sessionId) {
-        return "<html><body><video width='100%' height='100%' controls autoplay><source src='"
-                + Browserstack.videoUrl(sessionId)
-                + "' type='video/mp4'></video></body></html>";
     }
 
     @Attachment(value = "Page source", type = "text/plain")
