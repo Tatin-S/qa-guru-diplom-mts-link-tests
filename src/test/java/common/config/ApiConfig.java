56cb658f618d1baf64791ec5bee1999c4099fd08
Tatiana Stulova <vitadbnf@mail.ru>
2024-08-28 14:07:47 +0300

    change EventTests

--- src/test/java/common/config/ApiConfig.java
+++ src/test/java/common/config/ApiConfig.java
@@ -2,14 +2,6 @@ package common.config;
 
 import org.aeonbits.owner.Config;
 
-@Config.LoadPolicy(Config.LoadType.MERGE)
-@Config.Sources({
-        "system:properties",
-        "classpath:config/api.properties",
-        "file:~/api.properties",
-        "file:./api.properties"
-})
-
 public interface ApiConfig extends Config {
 
     @DefaultValue("https://userapi.mts-link.ru")
